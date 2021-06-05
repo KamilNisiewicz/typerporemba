ad3fed1da4d229e5d5d6440a1ea1ee414ea02b56
Kamil Nisiewicz <kamnisiewicz@gmail.com>
2021-06-05 11:42:00 +0200

    Added styling

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Bets;
 use App\Entity\Users;
+use App\Services\HelperServices;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -45,26 +48,125 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/user/details/{id}", name="user_results_details")
+     * @Route("/user/details/{userId}", name="user_results_details")
      */
-    public function userResultsDetails(int $id): Response
+    public function user(int $userId): Response
     {
 	$userInfo = $this->getDoctrine()
 	    ->getRepository(Users::class)
-	    ->findOneBy(['id' => $id]);
+	    ->findOneBy(['id' => $userId]);
     
 	$details = $this->getDoctrine()
 	    ->getRepository(Bets::class)
-	    ->getUserResultsDetails($id);
+	    ->getUserMatches($userId, 'finished');
 
 	$points = $this->getDoctrine()
 	    ->getRepository(Bets::class)
-	    ->sumUserPoints($id);
+	    ->sumUserPoints($userId);
 
         return $this->render('user/details.html.twig', [
 	    'userInfo' => $userInfo,
 	    'details' => $details,
 	    'points' => $points
         ]);
+    }
+
+    /**
+     * @Route("/user/bets", name="user_bets")
+     */
+    public function bets(): Response
+    {
+	$user = $this->getUser();
+	$userId = $user->getId();
+
+	$finished = $this->getDoctrine()
+	    ->getRepository(Bets::class)
+	    ->getUserMatches($userId, 'finished');
+
+	$today = $this->getDoctrine()
+	    ->getRepository(Bets::class)
+	    ->getUserMatches($userId, 'today');
+
+	$tomorrow = $this->getDoctrine()
+	    ->getRepository(Bets::class)
+	    ->getUserMatches($userId, 'tomorrow');
+
+	$rest = $this->getDoctrine()
+	    ->getRepository(Bets::class)
+	    ->getUserMatches($userId, 'rest');
+
+	return $this->render('user/bets.html.twig', [
+	    'finished' => $finished,
+	    'today' => $today,
+	    'tomorrow' => $tomorrow,
+	    'rest' => $rest,
+        ]);
+    }
+
+    /**
+     * @Route("/user/bet/{betId}", name="user_bet", methods={"GET"})
+     */
+    public function userBet(int $betId): Response
+    {
+	$user = $this->getUser();
+	$userId = $user->getId();
+
+	$bet = $this->getDoctrine()
+	    ->getRepository(Bets::class)
+	    ->getUserMatch($userId, $betId);
+	
+	if($bet){
+	    return $this->render('user/bet.html.twig', [
+		'bet' => $bet
+	    ]);
+	};
+	
+	return $this->redirectToRoute('user_bets');
+    }
+
+    /**
+     * @Route("/user/bet/{betId}", name="user_bet_action", methods={"POST"})
+     */
+    public function userBetAction(int $betId, Request $request, HelperServices $helperServices): Response
+    {
+	$em = $this->getDoctrine()->getManager();
+	$user = $this->getUser();
+	$userId = $user->getId();
+
+	$homeTeam = $helperServices->proper($request->get("home_team"));
+	$awayTeam = $helperServices->proper($request->get("away_team"));
+
+	if($userId && $betId && $homeTeam && $awayTeam){
+	    $bet = $this->getDoctrine()
+		->getRepository(Bets::class)
+		->findOneBy(
+		    [
+			'id' => $betId,
+			'user' => $userId
+		    ]
+		);
+
+	    if($bet){
+		$now = date('Y-m-d H:i:s');
+		$matchDate = $bet->getMatch()->getDate()->format('Y-m-d H:i:s');
+
+		if($matchDate > $now){
+		    $bet->setHomeTeamScore($homeTeam);
+		    $bet->setAwayTeamScore($awayTeam);
+		    $em->flush();
+		    $this->addFlash('success', 'Wynik poprawnie zmieniony!');
+		    $log = $now . ' User: ' . $userId . ' bet match: ' . $betId . ' : ' . $homeTeam . '-' . $awayTeam;
+		    file_put_contents('../var/log/bets.txt', "$log\n", FILE_APPEND);
+		}else{
+		    $this->addFlash('error', 'Za późno! Mecz już się rozpoczął!');
+		}
+	    }else{
+		$this->addFlash('error', 'Brak meczu o takim id dla tego użytkownika!');
+	    }
+	}else{
+	    $this->addFlash('error', 'Coś poszło nie tak, spróbuj jeszcze raz!');
+	}
+
+	return $this->redirectToRoute('user_bet', ['betId' => $betId]);
     }
 }
--- src/Repository/BetsRepository.php
+++ src/Repository/BetsRepository.php
@@ -35,7 +35,7 @@ class BetsRepository extends ServiceEntityRepository
 	    ->getArrayResult();
     }
 
-    public function sumUserPoints(int $id): array
+    public function sumUserPoints(int $userId): array
     {
 	$now = date("Y-m-d H:i:s");
 
@@ -44,16 +44,42 @@ class BetsRepository extends ServiceEntityRepository
 	    ->leftJoin(Matches::class, 'm', 'WITH', 'm.id = b.match')
 	    ->where('b.user = :id')
 	    ->andWhere('m.date < :date')
-	    ->setParameter('id', $id)
+	    ->setParameter('id', $userId)
 	    ->setParameter('date', $now)
 	    ->getQuery()
 	    ->getSingleResult();
     }
 
-    public function getUserResultsDetails(int $id): array
+    public function getUserMatches(int $userId, string $type): array
     {
 	$results = [];
-	$now = date("Y-m-d H:i:s");
+	$date = "";
+	$where = "";
+
+	switch ($type) {
+	    case 'finished':
+	    $date = date('Y-m-d H:i:s');
+	    $where = "m.date < '$date'";
+	    break;
+	    case 'today':
+	    $from = date('Y-m-d') . ' 00:00:00';
+	    $to   = date('Y-m-d') . ' 23:59:59';
+	    $where = "m.date BETWEEN '$from' AND '$to'";
+	    break;
+	    case 'tomorrow':
+	    $from = date('Y-m-d', strtotime('+1 day')) . ' 00:00:00';
+	    $to   = date('Y-m-d', strtotime('+1 day')) . ' 23:59:59';
+	    $where = "m.date BETWEEN '$from' AND '$to'";
+	    break;
+	    case 'rest':
+	    $from = date('Y-m-d', strtotime('+2 day')) . ' 00:00:00';
+	    $where = "m.date >= '$from'";
+	    break;
+	    case 'current':
+	    $date = date('Y-m-d H:i:s');
+	    $where = "m.date >= '$date'";
+	    break;
+	}
 
 	$results = $this->createQueryBuilder('b')
 	    ->select('t1.name as homeTeamName',
@@ -65,6 +91,7 @@ class BetsRepository extends ServiceEntityRepository
 		'b.homeTeamScore as userHomeTeamScore',
 		'b.awayTeamScore as userAwayTeamScore',
 		'b.points',
+		'b.id as betId',
 		't1.flagPath as homeTeamFlag',
 		't2.flagPath as awayTeamFlag'
 	    )
@@ -72,9 +99,40 @@ class BetsRepository extends ServiceEntityRepository
 	    ->leftJoin(Teams::class, 't1', 'WITH', 't1.id = m.homeTeam')
 	    ->leftJoin(Teams::class, 't2', 'WITH', 't2.id = m.awayTeam')
 	    ->where('b.user = :id')
-	    ->andWhere('m.date < :date')
-	    ->setParameter('id', $id)
-	    ->setParameter('date', $now)
+	    ->andWhere($where)
+	    ->setParameter('id', $userId)
+	    ->orderBy('m.date', 'ASC')
+	    ->getQuery()
+	    ->getArrayResult();
+	
+	return $results;
+    }
+
+    public function getUserMatch(int $userId, int $betId): array
+    {
+	$results = [];
+
+	$results = $this->createQueryBuilder('b')
+	    ->select('t1.name as homeTeamName',
+		't2.name as awayTeamName',
+		'm.phase',
+		'm.date',
+		'm.homeTeamScore',
+		'm.awayTeamScore',
+		'b.homeTeamScore as userHomeTeamScore',
+		'b.awayTeamScore as userAwayTeamScore',
+		'b.points',
+		'b.id as betId',
+		't1.flagPath as homeTeamFlag',
+		't2.flagPath as awayTeamFlag'
+	    )
+	    ->leftJoin(Matches::class, 'm', 'WITH', 'm.id = b.match')
+	    ->leftJoin(Teams::class, 't1', 'WITH', 't1.id = m.homeTeam')
+	    ->leftJoin(Teams::class, 't2', 'WITH', 't2.id = m.awayTeam')
+	    ->where('b.user = :id')
+	    ->andWhere('b.id = :betId')
+	    ->setParameter('id', $userId)
+	    ->setParameter('betId', $betId)
 	    ->orderBy('m.date', 'ASC')
 	    ->getQuery()
 	    ->getArrayResult();
