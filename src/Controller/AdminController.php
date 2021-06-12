43c67f5c850312294698d472719c18e38808b951
Kamil Nisiewicz <kamnisiewicz@gmail.com>
2021-06-12 15:10:54 +0200

    Added set score for empty matches in admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Matches;
 use App\Entity\Users;
+use App\Services\HelperServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -22,9 +25,36 @@ class AdminController extends AbstractController
             ->getRepository(Users::class)
             ->findBy(['active' => 0]);
 
+        $emptyMatches = $this->getDoctrine()
+            ->getRepository(Matches::class)
+            ->getEmptyMatches();
+
         return $this->render('admin/index.html.twig', [
             'activeUsers' => $activeUsers,
-            'inactiveUsers' => $inactiveUsers
+            'inactiveUsers' => $inactiveUsers,
+            'emptyMatches' => $emptyMatches
         ]);
+    }
+
+	/**
+	 * @Route("/admin/match/{matchId}", name="admin_match_action", methods={"POST"})
+	 */
+	public function userBetAction(int $matchId, Request $request, HelperServices $helperServices): Response
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$homeTeam = $helperServices->proper($request->get("home_team"));
+		$awayTeam = $helperServices->proper($request->get("away_team"));
+
+        $match = $this->getDoctrine()
+            ->getRepository(Matches::class)
+            ->findOneBy(
+                ['id' => $matchId]
+            );
+        
+        if($match){
+            print_r($match);
+        }
+        exit();
     }
 }
--- src/Repository/MatchesRepository.php
+++ src/Repository/MatchesRepository.php
@@ -41,4 +41,27 @@ class MatchesRepository extends ServiceEntityRepository
 
 		return $schedule;
 	}
+
+	public function getEmptyMatches()
+	{
+		$now = date('Y-m-d H:i:s');
+
+		return $this->createQueryBuilder('m')
+			->select(
+				't1.name as homeTeamName',
+				't2.name as awayTeamName',
+				'm.id',
+				'm.homeTeamScore',
+				'm.awayTeamScore',
+				't1.flagPath as homeTeamFlag',
+				't2.flagPath as awayTeamFlag'
+			)
+			->leftJoin(Teams::class, 't1', 'WITH', 't1.id = m.homeTeam')
+			->leftJoin(Teams::class, 't2', 'WITH', 't2.id = m.awayTeam')
+			->where('m.finished = false')
+			->andWhere('m.date < :date')
+			->setParameter('date', $now)
+			->getQuery()
+			->getArrayResult();
+	}
 }
